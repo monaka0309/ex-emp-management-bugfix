65b269b87a459069621e5f5ecc792e79e636f8a3
nakaryunosuke <naka.sava0309@gmail.com>
2024-11-22 13:43:58 +0900

    Add: Form とControllerの一部を作成。（一時保存）

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -3,6 +3,9 @@ package com.example.controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,7 +75,7 @@ public class AdministratorController {
 	 * @return ログイン画面へリダイレクト
 	 */
 	@PostMapping("/insert")
-	public String insert(InsertAdministratorForm form) {
+	public String insert(@Validated @ModelAttribute InsertAdministratorForm form, BindingResult result, Model model) {
 		Administrator administrator = new Administrator();
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
